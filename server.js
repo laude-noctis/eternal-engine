0a8259410204a89dfa06dfb277342081e0e5ac69
unknown <lizzybumpers11@gmail.com>
2024-01-09 15:50:25 -0600

    added some npm scripts/tried adding the handlebar logic to the server.js but may need the route handlers to get it working right?

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
