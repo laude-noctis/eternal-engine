d37d58b3429da33de7dc75b9488ae119b3792977
unknown <lizzybumpers11@gmail.com>
2024-01-10 10:59:05 -0600

    added the session storage with sequelize

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+// const routes = require('./controllers')
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
