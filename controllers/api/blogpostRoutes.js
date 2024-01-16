73f6e34d5afac44d6378c1a304dd6931df4853ae
unknown <lizzybumpers11@gmail.com>
2024-01-16 12:17:31 -0600

    created the api routing for deleting a blogpost

--- controllers/api/blogpostRoutes.js
+++ controllers/api/blogpostRoutes.js
@@ -2,7 +2,7 @@ const { blogPost } = require("../../models");
 const router = require("express").Router();
 const withAuth = require("../../utils/auth");
 
-router.post("/", withAuth, async (res, req) => {
+router.post("/", withAuth, async (req, res) => {
     try {
         const newBlogPost = await blogPost.create({
             ...req.body,
@@ -13,4 +13,15 @@ router.post("/", withAuth, async (res, req) => {
     } catch (err) {
         res.status(400).json(err)
     }
-})
+});
+
+router.delete("/:id", withAuth, async (req, res) => {
+    try {
+        const deleteBlogPost = await blogPost.destroy({
+            where: { id: req.params.id }
+        });
+        res.status(200).json(deleteBlogPost);
+    } catch (err) {
+        res.status(400).json({ error: "Failed to delete blog post :(" });
+    }
+});
