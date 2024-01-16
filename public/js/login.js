6bfb4e4e3aaa51089550dfb59952ab7f2d5b7cec
unknown <lizzybumpers11@gmail.com>
2024-01-16 11:14:09 -0600

    created the signup logic/fixed a couple typos

--- public/js/login.js
+++ public/js/login.js
@@ -1,18 +1,50 @@
 // Login logic
-const loginForm = async () => {
-    const email = document.getElementById("#email-login");
-    const password = document.getElementById("#password-login");
-    
+const loginForm = async (event) => {
+    event.preventDefault();
+
+    const email = document.getElementById("email-login");
+    const password = document.getElementById("password-login");
+
     if (email && password) {
-        const response = await fetch ("/api/users/login", {
+        const response = await fetch("/api/users/login", {
             method: "POST",
-            body: JSON.strigify({ email, password }),
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
         });
-    
+
         if (response.ok) {
             document.location.replace("/profile")
         } else {
             alert(response.statusText);
         };
     };
-}
+};
+
+document.querySelector(".login-form")
+document.addEventListener("submit", loginForm);
+
+// Signup Logic
+const signupForm = async (event) => {
+    event.preventDefault();
+
+    const name = document.getElementById("name-signup")
+    const email = document.getElementById("email-signup")
+    const password = document.getElementById("password-signup")
+
+    if (name && email && password) {
+        const response = await fetch("/api/users", {
+            method: "POST",
+            body: JSON.stringify({ name, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace("/profile");
+        } else {
+            alert(response.statusText)
+        };
+    };
+};
+
+document.querySelector(".signup-form");
+document.addEventListener("submit", signupForm)
