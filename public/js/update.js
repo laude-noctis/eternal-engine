cbad956e7c4835f77200ff6bab633c0f13810502
unknown <lizzybumpers11@gmail.com>
2024-01-19 15:59:39 -0600

    were getting there but now im not sure

--- public/js/update.js
+++ public/js/update.js
@@ -1,18 +1,23 @@
 const updatePost = async (event) => {
-    event.preventDefault();
+    if (event.target.hasAttribute("data-id")) {
+        const id = event.target.getAttribute("data-id")
 
-    const response = await fetch(`/api/blogposts/update/4`, {
-        method: "PUT",
-        body: JSON.stringify({}),
-        headers: { "Content-type": "application/json" }
-    });
+        const title = document.getElementById("post-title").value.trim();
+        const description = document.getElementById("post-description").value.trim();
 
-    if (response.ok) {
-        document.location.replace(`blogpost`)
-    } else {
-        alert("Failed to update blogpost")
+        const response = await fetch(`/api/blogposts/update/${id}`, {
+            method: "PUT",
+            body: JSON.stringify({ title, description }),
+            headers: { "Content-type": "application/json" }
+        });
+
+        if (response.ok) {
+            document.location.replace(`blogposts/${id}`)
+        } else (err) => {
+            console.error(err)
+            alert("Failed to update blogpost")
+        }
     }
 }
-
 
 document.querySelector("#update-post").addEventListener("click", updatePost)
