4f27d50a94a4fb3f335f5f1931ff940b3ac53a73
unknown <lizzybumpers11@gmail.com>
2024-01-20 09:29:20 -0600

    got username to show with the blogpost

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -17,7 +17,7 @@ router.get('/', async (req, res) => {
 
         res.render('homepage', {
             blogposts,
-            logged_in: req.session.logged_in
+            logged_in: req.session.logged_in,
         });
     } catch (err) {
         res.status(500).json(err);
