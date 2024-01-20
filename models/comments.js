7b58b617596f232bb10c6a6b2e3548bd2eb4d03d
unknown <lizzybumpers11@gmail.com>
2024-01-20 13:16:33 -0600

    created the comments model and exported it :)

---
+++ models/comments.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comments extends Model {}
+
+Comments.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        comment: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.DATE,
+        },
+        username: {
+            type: DataTypes.STRING,
+            references: {
+                model: "user",
+                key: "id",
+            },
+        },
+        blogpost_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "blogPost",
+                key: "id"
+            },
+        },
+
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comments',
+    },
+)
+
+module.exports = Comments
--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./user');
 const blogPost = require('./blogpost')
+const Comments = require('./comments')
 
 User.hasMany(blogPost, {
     foreignKey: 'user_id',
@@ -10,4 +11,8 @@ blogPost.belongsTo(User, {
     foreignKey: 'user_id'
 })
 
-module.exports = { User, blogPost };
+blogPost.hasMany(Comments, {
+    foreignKey: "comments_id"
+})
+
+module.exports = { User, blogPost, Comments };
